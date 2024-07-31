035a754764a61549d1d958375236c3f0454b9ebc
w <ghostoftheshell@mailbox.org>
2024-08-01 00:56:24 +0300

    Implement stringify function

--- value.c
+++ value.c
@@ -148,14 +148,96 @@ JV_DescribeToBuffer(JSONValue value, Buffer* buffer) {
 	*buffer = JV_Describe(value);
 }
 
+static void
+_StringifyToResizableBuffer(JSONValue, ResizableBuffer*);
 
 // encodes JSON structure to JSON string
 Buffer
 JV_Stringify(JSONValue value) {
-	// TODO:
+	ResizableBuffer rb = RB_Create(JSON_BUFFER_CAPACITY);
+	_StringifyToResizableBuffer(value, &rb);
+
+	return RB_ToBuffer(&rb);
 }
 
 void
 JV_StringifyToBuffer(JSONValue value, Buffer *buffer) {
-	// TODO:
+	*buffer = JV_Stringify(value);
 }
+
+static void
+_CB_AL_Stringify(size_t index, void* value, void* args) {
+	_CB_Args* cbArgs = (_CB_Args*) args;
+	ResizableBuffer *rb = (ResizableBuffer*) cbArgs->rb;
+		
+	if (!cbArgs->isFirst) {
+		RB_Append(rb, ",");
+	} else {
+		cbArgs->isFirst = false;
+	}
+
+	JSONValue jsonValue = *((JSONValue*) value);
+	_StringifyToResizableBuffer(jsonValue, rb);
+}
+
+static void
+_CB_SLM_Stringify(Buffer key, Buffer value, void* args) {
+	_CB_Args* cbArgs = (_CB_Args*) args;
+	ResizableBuffer *rb = (ResizableBuffer*) cbArgs->rb;
+		
+	if (!cbArgs->isFirst) {
+		RB_Append(rb, ",");
+	} else {
+		cbArgs->isFirst = false;
+	}
+
+	RB_Append(rb, "\"");
+	RB_Append(rb, key);
+	RB_Append(rb, "\"");
+	RB_Append(rb, ":");
+
+	JSONValue jsonValue = *((JSONValue*) value.data);
+	_StringifyToResizableBuffer(jsonValue, rb);
+}
+
+static void
+_StringifyToResizableBuffer(JSONValue value, ResizableBuffer* rb) {
+	switch (value.type) {
+	case JSON_STRING:
+		RB_Append(rb, "\"");
+		RB_Append(rb, value.str);
+		RB_Append(rb, "\"");
+		break;
+	case JSON_INT:
+		RB_Append(rb, S_From(value.integer));
+		break;
+	case JSON_FLOAT:
+		RB_Append(rb, S_From(value.floating));
+		break;
+	case JSON_BOOL:
+		RB_Append(rb, S_From(value.boolean));
+		break;
+	case JSON_OBJECT:
+		RB_Append(rb, "{");
+		_CB_Args args = {
+			.isFirst = true,
+			.rb = rb,
+		};
+		SLM_Iterate(value.object.slm, _CB_SLM_Stringify, &args);
+		RB_Append(rb, "}");
+		break;
+	case JSON_ARRAY:
+		RB_Append(rb, "[");
+		args = (_CB_Args) {
+			.isFirst = true,
+			.rb = rb,
+		};
+		AL_Iterate(value.array.al, _CB_AL_Stringify, &args);
+		RB_Append(rb, "]");
+		break;
+	case JSON_NULL:
+		RB_Append(rb, "null");
+		break;
+	}
+}
+
